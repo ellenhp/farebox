9f31e64c04bed3d4bc2029e4741ecbd5247001ed
Ellen Poe <ellen@ellenhp.me>
2025-03-28 11:00:21 -0700

    refactor: cleanup transfer calculations

--- src/valhalla/mod.rs
+++ src/valhalla/mod.rs
@@ -1,7 +1,9 @@
-use anyhow::Ok;
-use log::debug;
+use anyhow::bail;
+use log::{debug, warn};
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
+use std::{result::Result::Ok, time::Duration};
+use tokio::time::sleep;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct MatrixRequest {
@@ -30,6 +32,12 @@ pub struct MatrixLineItem {
     pub from_index: Option<usize>,
 }
 
+async fn matrix_request_inner(client: &Client, url: &str) -> Result<MatrixResponse, anyhow::Error> {
+    debug!("Sending matrix request: {}", url);
+    let body = client.get(url).send().await?.text().await?;
+    return Ok(serde_json::from_str(&body)?);
+}
+
 pub async fn matrix_request(
     client: &Client,
     endpoint: &str,
@@ -40,7 +48,15 @@ pub async fn matrix_request(
         endpoint,
         serde_json::to_string(&request)?
     );
-    debug!("Sending matrix request: {}", url);
-    let body = client.get(url).send().await?.text().await?;
-    Ok(serde_json::from_str(&body)?)
+    let retries = 5;
+    for retry in 1..=retries {
+        if let Ok(response) = matrix_request_inner(client, &url).await {
+            return Ok(response);
+        } else {
+            warn!("Valhalla error. Retries remaining: {}", retries - retry);
+            sleep(Duration::from_millis(100)).await;
+            continue;
+        }
+    }
+    bail!("Valhalla error")
 }
