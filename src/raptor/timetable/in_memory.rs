9b89c10f3f8cf96ab45b1a7096c93508678668b2
Ellen Poe <ellen@ellenhp.me>
2025-03-28 11:19:03 -0700

    refactor: slightly clean up timetable preprocessing

--- src/raptor/timetable/in_memory.rs
+++ src/raptor/timetable/in_memory.rs
@@ -4,9 +4,9 @@ use std::{
 };
 
 use anyhow::bail;
-use chrono::{Days, Local, TimeDelta, TimeZone};
+use chrono::{Days, Local, NaiveDate, TimeDelta, TimeZone};
 use chrono_tz::Tz;
-use gtfs_structures::Gtfs;
+use gtfs_structures::{Agency, Gtfs};
 use log::{debug, warn};
 use rstar::RTree;
 use s2::{cellid::CellID, latlng::LatLng};
@@ -152,8 +152,8 @@ pub struct InMemoryTimetableBuilder {
 }
 
 impl<'a> InMemoryTimetableBuilder {
-    pub fn new() -> InMemoryTimetableBuilder {
-        InMemoryTimetableBuilder {
+    pub fn new(gtfs: &Gtfs) -> Result<Self, anyhow::Error> {
+        let mut builder = InMemoryTimetableBuilder {
             next_stop_id: 0,
             next_stop_route_id: 0,
             next_route_id: 0,
@@ -161,11 +161,13 @@ impl<'a> InMemoryTimetableBuilder {
             next_route_stop_id: 0,
             next_trip_stop_time_id: 0,
             timetable: InMemoryTimetable::new(),
-        }
+        };
+        builder.preprocess_gtfs(gtfs)?;
+        Ok(builder)
     }
 
-    pub fn preprocess_gtfs(&mut self, gtfs: &Gtfs) -> Result<(), anyhow::Error> {
-        let agencies: HashMap<String, _> = gtfs
+    fn preprocess_gtfs(&mut self, gtfs: &Gtfs) -> Result<(), anyhow::Error> {
+        let agencies: HashMap<String, &Agency> = gtfs
             .agencies
             .iter()
             .map(|agency| (agency.id.clone().unwrap_or(String::new()), agency))
@@ -275,168 +277,201 @@ impl<'a> InMemoryTimetableBuilder {
 
         debug!("Done sorting");
 
-        {
-            for (route_id, route_stop_list) in route_id_to_route_map.iter() {
-                let route = Route {
-                    route_index: *route_id,
-                    first_route_stop: self.next_route_stop_id,
-                    first_route_trip: self.next_route_trip_id,
-                };
-                self.timetable
-                    .route_shapes
-                    .insert(route, route_shapes[route_id].clone());
-                self.timetable.routes.push(route);
+        self.process_routes_trips(
+            gtfs,
+            start_date,
+            &agencies,
+            &route_id_to_route_map,
+            &route_id_to_stop_distances,
+            &route_id_to_trip_list,
+            &route_shapes,
+        );
 
-                for (stop_seq, stop_id) in route_stop_list.iter().enumerate() {
-                    self.timetable.route_stops.push(RouteStop {
-                        route_index: *route_id,
-                        stop_index: *stop_id,
-                        stop_seq: stop_seq as u32,
-                        distance_along_route: route_id_to_stop_distances[route_id][stop_seq],
-                    });
-                    self.next_route_stop_id += 1;
-                }
-                let trips_pre_sort = route_id_to_trip_list.get(&route_id).unwrap().clone();
-                let mut trips = trips_pre_sort.clone();
-                trips.sort_by_cached_key(|(day, gtfs_trip_id)| {
-                    gtfs.get_trip(gtfs_trip_id)
-                        .unwrap()
-                        .stop_times
-                        .iter()
-                        .filter_map(|stop_time| stop_time.departure_time)
-                        .next()
-                        .unwrap_or(u32::MAX)
-                        // TODO: This is a DST bug.
-                        .saturating_add(*day as u32 * 3600 * 24)
-                });
-                for (day, gtfs_trip_id) in &trips {
-                    let trip_agency_id = gtfs.routes[&gtfs.trips[gtfs_trip_id].route_id]
-                        .agency_id
-                        .clone()
-                        .unwrap_or(String::new());
-                    let trip_agency = if let Some(agency) = agencies.get(&trip_agency_id) {
-                        agency
-                    } else {
-                        if agencies.len() == 1 {
-                            agencies.values().next().unwrap()
-                        } else {
-                            warn!("No matching agency: {}, {:?}", trip_agency_id, agencies);
-                            continue;
-                        }
-                    };
-                    let agency_tz: Tz = trip_agency.timezone.parse().unwrap();
-                    let first_trip_stop_time = self.next_trip_stop_time_id;
+        self.process_stops(
+            gtfs,
+            &route_id_to_route_map,
+            &stop_id_to_stop_map,
+            &stop_id_to_route_list,
+        )?;
 
-                    let trip = gtfs.get_trip(gtfs_trip_id).unwrap();
-
-                    #[cfg(feature = "enforce_invariants")]
-                    let mut prev_time = 0u32;
-                    for (stop_seq, stop_time) in trip.stop_times.iter().enumerate() {
-                        #[cfg(feature = "enforce_invariants")]
-                        if let Some(arrival_time) = stop_time.arrival_time {
-                            assert!(arrival_time >= prev_time);
-                            prev_time = arrival_time;
-                        }
-                        let day_start = agency_tz
-                            .from_local_datetime(
-                                &start_date
-                                    .checked_add_days(Days::new(*day as u64))
-                                    .unwrap()
-                                    // GTFS "Time" fields are measured from noon minus 12hrs.
-                                    .and_hms_opt(12, 0, 0)
-                                    .unwrap(),
-                            )
-                            .unwrap()
-                            // GTFS "Time" fields are measured from noon minus 12hr.
-                            .checked_sub_signed(TimeDelta::hours(12))
-                            .unwrap();
-
-                        let arrival_time = day_start
-                            .checked_add_signed(TimeDelta::seconds(
-                                stop_time.arrival_time.unwrap_or(u32::MAX) as i64,
-                            ))
-                            .unwrap();
-                        let departure_time = day_start
-                            .checked_add_signed(TimeDelta::seconds(
-                                stop_time.departure_time.unwrap_or(u32::MAX) as i64,
-                            ))
-                            .unwrap();
-                        self.timetable.trip_stop_times.push(TripStopTime::new(
-                            self.next_route_trip_id,
-                            stop_seq,
-                            arrival_time,
-                            departure_time,
-                        ));
-                        self.next_trip_stop_time_id += 1;
-                    }
-                    let trip = Trip {
-                        trip_index: self.next_route_trip_id,
-                        route_index: *route_id,
-                        first_trip_stop_time,
-                        last_trip_stop_time: self.next_trip_stop_time_id,
-                    };
-                    self.timetable.route_trips.push(trip);
-                    let agency_name = trip_agency.name.clone();
-                    let metadata = TripMetadata {
-                        agency_name: Some(agency_name),
-                        headsign: gtfs.trips[gtfs_trip_id].clone().trip_headsign,
-                        route_name: gtfs.routes[&gtfs.trips[gtfs_trip_id].route_id]
-                            .short_name
-                            .clone(),
-                    };
-                    self.timetable.trip_metadata_map.insert(trip, metadata);
-
-                    self.next_route_trip_id += 1;
-                }
-            }
-        }
-
-        {
-            for (stop_id, gtfs_stop_id) in stop_id_to_stop_map.iter() {
-                let gtfs_stop = gtfs.get_stop(&gtfs_stop_id).unwrap();
-                let lat = if let Some(lat) = gtfs_stop.latitude {
-                    lat
-                } else {
-                    bail!("Can't process feeds containing stop IDs without lat/lng")
-                };
-                let lng = if let Some(lng) = gtfs_stop.longitude {
-                    lng
-                } else {
-                    bail!("Can't process feeds containing stop IDs without lat/lng")
-                };
-                let s2cell: CellID = LatLng::from_degrees(lat, lng).into();
-                let stop = Stop {
-                    stop_index: *stop_id,
-                    s2cell: s2cell.0,
-                    first_stop_route_index: self.next_stop_route_id,
-                };
-                self.timetable.stops.push(stop);
-                self.timetable
-                    .stop_metadata_map
-                    .insert(stop, gtfs_stop.clone());
-                // TODO: Feed ID.
-                for route in stop_id_to_route_list
-                    .get(&stop_id)
-                    .unwrap_or(&BTreeSet::new())
-                {
-                    let mut seq = 0usize;
-                    let mut found_seq = false;
-                    for route_stop_seq_candidate in route_id_to_route_map.get(route).unwrap() {
-                        if stop_id == route_stop_seq_candidate {
-                            found_seq = true;
-                            break;
-                        }
-                        seq += 1;
-                    }
-                    assert!(found_seq);
-                    self.timetable.stop_routes.push(StopRoute {
-                        route_index: *route,
-                        stop_seq: seq,
-                    });
-                    self.next_stop_route_id += 1;
-                }
-            }
-        }
         Result::Ok(())
+    }
+
+    fn process_routes_trips(
+        &mut self,
+        gtfs: &Gtfs,
+        start_date: NaiveDate,
+        agencies: &HashMap<String, &Agency>,
+        route_id_to_route_map: &BTreeMap<usize, Vec<usize>>,
+        route_id_to_stop_distances: &BTreeMap<usize, Vec<f32>>,
+        route_id_to_trip_list: &BTreeMap<usize, Vec<(u16, String)>>,
+        route_shapes: &BTreeMap<usize, Option<Vec<ShapeCoordinate>>>,
+    ) {
+        for (route_id, route_stop_list) in route_id_to_route_map.iter() {
+            let route = Route {
+                route_index: *route_id,
+                first_route_stop: self.next_route_stop_id,
+                first_route_trip: self.next_route_trip_id,
+            };
+            self.timetable
+                .route_shapes
+                .insert(route, route_shapes[route_id].clone());
+            self.timetable.routes.push(route);
+
+            for (stop_seq, stop_id) in route_stop_list.iter().enumerate() {
+                self.timetable.route_stops.push(RouteStop {
+                    route_index: *route_id,
+                    stop_index: *stop_id,
+                    stop_seq: stop_seq as u32,
+                    distance_along_route: route_id_to_stop_distances[route_id][stop_seq],
+                });
+                self.next_route_stop_id += 1;
+            }
+            let trips_pre_sort = route_id_to_trip_list.get(&route_id).unwrap().clone();
+            let mut trips = trips_pre_sort.clone();
+            trips.sort_by_cached_key(|(day, gtfs_trip_id)| {
+                gtfs.get_trip(gtfs_trip_id)
+                    .unwrap()
+                    .stop_times
+                    .iter()
+                    .filter_map(|stop_time| stop_time.departure_time)
+                    .next()
+                    .unwrap_or(u32::MAX)
+                    // TODO: This is a DST bug.
+                    .saturating_add(*day as u32 * 3600 * 24)
+            });
+            for (day, gtfs_trip_id) in &trips {
+                let trip_agency_id = gtfs.routes[&gtfs.trips[gtfs_trip_id].route_id]
+                    .agency_id
+                    .clone()
+                    .unwrap_or(String::new());
+                let trip_agency = if let Some(agency) = agencies.get(&trip_agency_id) {
+                    agency
+                } else {
+                    if agencies.len() == 1 {
+                        agencies.values().next().unwrap()
+                    } else {
+                        warn!("No matching agency: {}, {:?}", trip_agency_id, agencies);
+                        continue;
+                    }
+                };
+                let agency_tz: Tz = trip_agency.timezone.parse().unwrap();
+                let first_trip_stop_time = self.next_trip_stop_time_id;
+
+                let trip = gtfs.get_trip(gtfs_trip_id).unwrap();
+
+                #[cfg(feature = "enforce_invariants")]
+                let mut prev_time = 0u32;
+                for (stop_seq, stop_time) in trip.stop_times.iter().enumerate() {
+                    #[cfg(feature = "enforce_invariants")]
+                    if let Some(arrival_time) = stop_time.arrival_time {
+                        assert!(arrival_time >= prev_time);
+                        prev_time = arrival_time;
+                    }
+                    let day_start = agency_tz
+                        .from_local_datetime(
+                            &start_date
+                                .checked_add_days(Days::new(*day as u64))
+                                .unwrap()
+                                // GTFS "Time" fields are measured from noon minus 12hrs.
+                                .and_hms_opt(12, 0, 0)
+                                .unwrap(),
+                        )
+                        .unwrap()
+                        // GTFS "Time" fields are measured from noon minus 12hr.
+                        .checked_sub_signed(TimeDelta::hours(12))
+                        .unwrap();
+
+                    let arrival_time = day_start
+                        .checked_add_signed(TimeDelta::seconds(
+                            stop_time.arrival_time.unwrap_or(u32::MAX) as i64,
+                        ))
+                        .unwrap();
+                    let departure_time = day_start
+                        .checked_add_signed(TimeDelta::seconds(
+                            stop_time.departure_time.unwrap_or(u32::MAX) as i64,
+                        ))
+                        .unwrap();
+                    self.timetable.trip_stop_times.push(TripStopTime::new(
+                        self.next_route_trip_id,
+                        stop_seq,
+                        arrival_time,
+                        departure_time,
+                    ));
+                    self.next_trip_stop_time_id += 1;
+                }
+                let trip = Trip {
+                    trip_index: self.next_route_trip_id,
+                    route_index: *route_id,
+                    first_trip_stop_time,
+                    last_trip_stop_time: self.next_trip_stop_time_id,
+                };
+                self.timetable.route_trips.push(trip);
+                let agency_name = trip_agency.name.clone();
+                let metadata = TripMetadata {
+                    agency_name: Some(agency_name),
+                    headsign: gtfs.trips[gtfs_trip_id].clone().trip_headsign,
+                    route_name: gtfs.routes[&gtfs.trips[gtfs_trip_id].route_id]
+                        .short_name
+                        .clone(),
+                };
+                self.timetable.trip_metadata_map.insert(trip, metadata);
+
+                self.next_route_trip_id += 1;
+            }
+        }
+    }
+
+    fn process_stops(
+        &mut self,
+        gtfs: &Gtfs,
+        route_id_to_route_map: &BTreeMap<usize, Vec<usize>>,
+        stop_id_to_stop_map: &BTreeMap<usize, String>,
+        stop_id_to_route_list: &BTreeMap<usize, BTreeSet<usize>>,
+    ) -> Result<(), anyhow::Error> {
+        for (stop_id, gtfs_stop_id) in stop_id_to_stop_map.iter() {
+            let gtfs_stop = gtfs.get_stop(&gtfs_stop_id).unwrap();
+            let lat = if let Some(lat) = gtfs_stop.latitude {
+                lat
+            } else {
+                bail!("Can't process feeds containing stop IDs without lat/lng")
+            };
+            let lng = if let Some(lng) = gtfs_stop.longitude {
+                lng
+            } else {
+                bail!("Can't process feeds containing stop IDs without lat/lng")
+            };
+            let s2cell: CellID = LatLng::from_degrees(lat, lng).into();
+            let stop = Stop {
+                stop_index: *stop_id,
+                s2cell: s2cell.0,
+                first_stop_route_index: self.next_stop_route_id,
+            };
+            self.timetable.stops.push(stop);
+            self.timetable
+                .stop_metadata_map
+                .insert(stop, gtfs_stop.clone());
+            for route in stop_id_to_route_list
+                .get(&stop_id)
+                .unwrap_or(&BTreeSet::new())
+            {
+                let mut seq = 0usize;
+                let mut found_seq = false;
+                for route_stop_seq_candidate in route_id_to_route_map.get(route).unwrap() {
+                    if stop_id == route_stop_seq_candidate {
+                        found_seq = true;
+                        break;
+                    }
+                    seq += 1;
+                }
+                assert!(found_seq);
+                self.timetable.stop_routes.push(StopRoute {
+                    route_index: *route,
+                    stop_seq: seq,
+                });
+                self.next_stop_route_id += 1;
+            }
+        }
+        Ok(())
     }
 }
