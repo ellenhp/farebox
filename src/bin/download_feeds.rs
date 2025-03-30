eb35d82346ee2292bed5a2dce6bada8b926543ff
Ellen Poe <ellen@ellenhp.me>
2025-03-30 08:00:51 -0700

    fix: redo error handling and concurrency

--- src/bin/download_feeds.rs
+++ src/bin/download_feeds.rs
@@ -1,8 +1,9 @@
-use std::{ffi::OsString, path::PathBuf, str::FromStr};
+use std::{ffi::OsString, path::PathBuf, str::FromStr, time::Duration};
 
 use clap::Parser;
 use farebox::dmfr::DistributedMobilityFeedRegistry;
 use log::{debug, info};
+use reqwest::Client;
 use tokio::{fs, spawn};
 
 #[derive(Parser)]
@@ -14,13 +15,17 @@ struct Args {
 }
 
 async fn download_dmfr(path: PathBuf, zip_dir: PathBuf) -> Result<(), anyhow::Error> {
+    let client = Client::builder()
+        .timeout(Duration::from_secs(10))
+        .build()
+        .unwrap();
     debug!("Enumerating feeds from {:?}", &path);
     let dmfr: DistributedMobilityFeedRegistry =
         serde_json::from_str(&fs::read_to_string(&path).await?)?;
     for (feed_idx, feed) in dmfr.feeds.iter().enumerate() {
         if let Some(url) = &feed.urls.static_current {
             debug!("Downloading feed from: {:?}", url);
-            let response = reqwest::get(url.as_str()).await?;
+            let response = client.get(url.as_str()).send().await?;
             let mut filename = path
                 .file_name()
                 .expect("GTFS feed not a file")
@@ -57,14 +62,18 @@ async fn main() -> Result<(), anyhow::Error> {
             .clone()
             .unwrap_or(args.dmfr_dir.clone())
             .clone();
-        let handle = spawn(async move { download_dmfr(path, zip_dir) }).await?;
+        let handle = spawn(async move { download_dmfr(path, zip_dir) });
         handles.push(handle);
     }
     for handle in handles {
         match handle.await {
-            Ok(_) => {}
+            Ok(future) => {
+                if let Err(err) = future.await {
+                    log::warn!("Failed to download a feed: {err}")
+                }
+            }
             Err(err) => {
-                log::warn!("Failed to download a feed: {err}")
+                log::warn!("Failed to join task: {err}")
             }
         }
     }
